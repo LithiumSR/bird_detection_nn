27fde2dfdec912356b50550ddddefadaf8fc1c84
Leonardo Sarra <leonardosarra@outlook.com>
2019-05-06 15:36:13 +0200

    Common: Improve APIs for getting and plotting audio data

--- common.py
+++ common.py
@@ -14,24 +14,37 @@ class Utils:
         librosa.output.write_wav(file_path, data, sr)
 
     @staticmethod
+    def get_plot_data(data, sr, graph_type):
+        if graph_type == "melspectrogram":
+            S = librosa.feature.melspectrogram(y=data, sr=sr)
+            return librosa.power_to_db(S, ref=np.max)
+
+        elif graph_type == "melspectrogram-energy":
+            S = librosa.feature.melspectrogram(y=data, sr=sr)
+            return librosa.amplitude_to_db(S, ref=np.max)
+
+        elif graph_type == "spectrogram":
+            stft = librosa.core.spectrum.stft(data, hop_length=512)
+            return librosa.amplitude_to_db(stft, ref=np.max)
+
+        elif graph_type == "filterbank":
+            S = librosa.feature.melspectrogram(y=data, sr=sr, hop_length=512)
+            return librosa.core.amplitude_to_db(S, ref=np.max)
+
+    @staticmethod
     def write_graph(data, sr, file_path, graph_type):
         plt.figure(figsize=(10, 4))
         if graph_type == "melspectrogram":
-            S = librosa.feature.melspectrogram(y=data, sr=sr)
-            librosa.display.specshow(librosa.power_to_db(S, ref=np.max), sr=sr,
+            librosa.display.specshow(data, sr=sr,
                                      y_axis='mel', fmax=8000, x_axis='time', ax=None)
         elif graph_type == "melspectrogram-energy":
-            S = librosa.feature.melspectrogram(y=data, sr=sr)
-            librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), sr=sr,
+            librosa.display.specshow(data, sr=sr,
                                      y_axis='mel', fmax=8000, x_axis='time', ax=None)
         elif graph_type == "spectrogram":
-            stft = librosa.core.spectrum.stft(data, hop_length=512)
-            librosa.display.specshow(librosa.amplitude_to_db(stft, ref=np.max), sr=sr, ax=None, y_axis='log',
+            librosa.display.specshow(data, sr=sr, ax=None, y_axis='log',
                                      x_axis='time')
         elif graph_type == "filterbank":
-            S = librosa.feature.melspectrogram(y=data, sr=sr,hop_length=512)
-            librosa.display.specshow(librosa.core.amplitude_to_db(S, ref=np.max), sr=sr, ax=None, y_axis='log', hop_length=512,
-                                     x_axis='frames')
+            librosa.display.specshow(data, sr=sr, ax=None, y_axis='log', hop_length=512, x_axis='frames')
         plt.margins(0)
         ax = plt.axes()
         ax.spines['left'].set_visible(False)
