cdeb6026f7913cecee09159a2f3d3410f999df47
Leonardo Sarra <leonardosarra@outlook.com>
2019-06-14 10:57:55 +0200

    NetworkLearner: Fix arg parser, add support for custom output filenames

--- NetworkLearner.py
+++ NetworkLearner.py
@@ -1,26 +1,32 @@
 import argparse
 
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.optimizers import SGD
 
 from DataParser import DataParser
 
 
 class DataLearner:
-    def __init__(self, neural_network, data_parser, epochs=30, early_stopping=False, save_best_checkpoint=True,
-                 use_validation_set=False):
+    def __init__(self, neural_network, data_parser, epochs=30, early_stopping=True, save_best_checkpoint=True,
+                 use_validation_set=False, output=None):
         self.neural_network = neural_network
         self.epochs = epochs
         self.data_parser = data_parser
         self.early_stopping = early_stopping
         self.save_best_checkpoint = save_best_checkpoint
         self.use_validation_set = use_validation_set
+        self.output = output
 
     def save(self, model):
         model.save(self.get_model_save_name())
 
     def get_model_save_name(self, checkpoint=False):
-        ret = self.neural_network + "_" + self.data_parser.graph_type + ".h5"
+        if self.output is None:
+            ret = self.neural_network + "_" + self.data_parser.graph_type + ".h5"
+        else:
+            ret = self.output
+        if not ret.endswith(".h5"):
+            ret += ".h5"
+
         if checkpoint:
             return "best_" + ret
         return ret
@@ -36,8 +42,8 @@ class DataLearner:
             val_steps = len(self.data_parser.val_graph_files_name) // self.data_parser.batch_size
 
         cb = []
-        if self.early_stopping:
-            cb.append(EarlyStopping(monitor='loss', mode='min', verbose=1))
+        if self.early_stopping and self.use_validation_set:
+            cb.append(EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=2))
 
         if self.save_best_checkpoint:
             cb.append(ModelCheckpoint(self.get_model_save_name(checkpoint=True), monitor='loss', mode='min',
@@ -55,13 +61,17 @@ class DataLearner:
         elif self.neural_network == "leonetv2":
             from models import LeoNetV2
             model = LeoNetV2.LeoNetV2_model((1, 224, 224, 3))
-            history = model.fit_generator(generator=generator, validation_data=generator_val, validation_steps=val_steps,
-                                epochs=self.epochs, steps_per_epoch=steps, callbacks=cb)
+            history = model.fit_generator(generator=generator, validation_data=generator_val,
+                                          validation_steps=val_steps,
+                                          epochs=self.epochs, steps_per_epoch=steps, callbacks=cb)
             self.print_history(history)
             return model
 
     def print_history(self, history):
         import matplotlib.pyplot as plt
+        name = self.get_model_save_name()
+        if self.output is not None:
+            name = self.get_model_save_name()
         plt.plot(history.history['acc'])
         plt.title('model accuracy')
         plt.ylabel('accuracy')
@@ -69,7 +79,9 @@ class DataLearner:
             plt.plot(history.history['val_loss'])
         plt.xlabel('epoch')
         plt.legend(['train', 'test'], loc='upper left')
+        plt.savefig(name + "_acc.png")
         plt.show()
+
         # summarize history for loss
         plt.plot(history.history['loss'])
         if self.use_validation_set:
@@ -78,12 +90,14 @@ class DataLearner:
         plt.ylabel('loss')
         plt.xlabel('epoch')
         plt.legend(['train', 'test'], loc='upper left')
+        plt.savefig(name + "_loss.png")
         plt.show()
 
 
-def main(neural_network, type_graph, folders, batch_size, val_percentage):
+def main(neural_network, type_graph, folders, epochs, batch_size, val_percentage, output):
     data_parser = DataParser("training", folders, type_graph, batch_size=batch_size, val_percentage=val_percentage)
-    learner = DataLearner(neural_network, data_parser, use_validation_set=False if val_percentage == 0.0 else True)
+    learner = DataLearner(neural_network, data_parser, epochs=epochs,
+                          use_validation_set=False if val_percentage == 0.0 else True, output=output)
     model = learner.train()
     learner.save(model)
     return
@@ -91,16 +105,20 @@ def main(neural_network, type_graph, folders, batch_size, val_percentage):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Customization options for the data learner")
-    parser.add_argument("type_graph", nargs='?', default="melspectrogram", help='Set type of graph')
-    parser.add_argument("neural_network", nargs='?', default="leonetv2", help='Set nn type')
-    parser.add_argument("folders", nargs='?', type=list, default=["ff1010bird", "BirdVoxDCASE20k"],
+    parser.add_argument("type_graph", nargs='?', default="spectrogram", help='Set type of graph')
+    parser.add_argument("neural_network", nargs='?', default="leonet", help='Set nn type')
+    parser.add_argument("folders", nargs='?', default="BirdVoxDCASE20k,ff1010bird",
                         help='Set of folders that will be used as the source of the graphs')
-    parser.add_argument("batch_size", nargs='?', default=20,
+    parser.add_argument("batch_size", nargs='?', type=int, default=20,
                         help='Batch size of the files used to train the model')
-    parser.add_argument("validation_percentage", nargs='?', default=0.0,
+    parser.add_argument("validation_percentage", nargs='?', type=float, default=0.15,
                         help='Batch size of the files used to train the model')
-    parser.add_argument("epochs", nargs='?', default=1,
+    parser.add_argument("epochs", nargs='?', type=int, default=1,
                         help='Number of epochs')
+    parser.add_argument("output", nargs='?', default="test2",
+                        help='Output filename')
 
     args = parser.parse_args()
-    main(args.neural_network, args.type_graph, args.folders, args.batch_size, args.validation_percentage)
+    print(args)
+    main(args.neural_network, args.type_graph, [item for item in args.folders.split(',')], args.epochs, args.batch_size, args.validation_percentage,
+         args.output)
